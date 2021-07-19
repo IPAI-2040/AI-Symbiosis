7bc54fbd7b36dd8b768aa2ccca9d02e7fbbeb137
andrew <tenketech@gmail.com>
2021-07-19 18:05:35 +0800

    daily update

--- android/AISymbiosis/context/src/main/java/cn/darkfog/ai/symbiosis/context/processor/RouteProcessor.kt
+++ android/AISymbiosis/context/src/main/java/cn/darkfog/ai/symbiosis/context/processor/RouteProcessor.kt
@@ -3,7 +3,6 @@ package cn.darkfog.ai.symbiosis.context.processor
 import com.google.auto.service.AutoService
 import javax.annotation.processing.AbstractPrcocess
 
-@AutoService(Processor:class)
 class RouteProcessor:AbstractPrcocess(){
 
 }
