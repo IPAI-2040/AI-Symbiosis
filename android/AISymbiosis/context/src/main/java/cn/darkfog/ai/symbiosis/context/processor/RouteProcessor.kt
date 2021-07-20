a56a3aada84bbc2823c6f5cf36be79582b19bb56
andrew <tenketech@gmail.com>
2021-07-20 13:11:23 +0800

    update jdk

--- android/AISymbiosis/context/src/main/java/cn/darkfog/ai/symbiosis/context/processor/RouteProcessor.kt
+++ android/AISymbiosis/context/src/main/java/cn/darkfog/ai/symbiosis/context/processor/RouteProcessor.kt
@@ -1,8 +1,8 @@
 package cn.darkfog.ai.symbiosis.context.processor
 
 import com.google.auto.service.AutoService
-import javax.annotation.processing.AbstractPrcocess
+import javax.annotation.processing.AbstractProcessor
 
-class RouteProcessor:AbstractPrcocess(){
+class RouteProcessor:AbstractProcessor(){
 
 }
