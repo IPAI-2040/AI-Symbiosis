0ec644814ae3fdf2f3a98165c2a2f93041b13c4e
andrew <tenketech@gmail.com>
2021-07-20 18:42:10 +0800

    daily update

--- android/AISymbiosis/context/src/main/java/cn/darkfog/ai/symbiosis/context/processor/RouteProcessor.kt
+++ android/AISymbiosis/context/src/main/java/cn/darkfog/ai/symbiosis/context/processor/RouteProcessor.kt
@@ -1,8 +1,66 @@
 package cn.darkfog.ai.symbiosis.context.processor
 
+import cn.darkfog.ai.symbiosis.context.annotation.Action
+import cn.darkfog.ai.symbiosis.context.annotation.Domain
 import com.google.auto.service.AutoService
-import javax.annotation.processing.AbstractProcessor
+import com.squareup.kotlinpoet.ClassName
+import java.lang.Exception
+import java.lang.reflect.AnnotatedElement
+import javax.annotation.processing.*
+import javax.lang.model.SourceVersion
+import javax.lang.model.element.Element
+import javax.lang.model.element.TypeElement
+import javax.lang.model.util.Elements
 
 class RouteProcessor:AbstractProcessor(){
+    private lateinit var filer: Filer
+    private lateinit var messager: Messager
+    private lateinit var elementUtils:Elements
+
+    override fun getSupportedAnnotationTypes(): MutableSet<String> {
+        return hashSetOf(
+            Domain::class.java.canonicalName,
+        )
+    }
+
+    override fun getSupportedSourceVersion(): SourceVersion {
+        return SourceVersion.latestSupported()
+    }
+
+    override fun init(processingEnv: ProcessingEnvironment) {
+        super.init(processingEnv)
+        filer = processingEnv.filer
+        messager = processingEnv.messager
+        elementUtils = processingEnv.elementUtils
+    }
+
+
+    override fun process(annotations: MutableSet<out TypeElement>,
+                         roundEnvironment: RoundEnvironment): Boolean {
+        val domainElements = roundEnvironment.getElementsAnnotatedWith(Domain::class.java)
+        val actionElements = roundEnvironment.getElementsAnnotatedWith(Action::class.java)
+
+        val domainMap = hashMapOf<String,MutableList<Element>>()
+        domainElements.forEach { domain ->
+            val domainName = domain.getAnnotation(Domain::class.java).name
+            if (domainMap.containsKey(domainName)){
+                throw Exception("Duplicated Name: $domain")
+            }
+            domainMap[domainName] = mutableListOf()
+        }
+
+        actionElements.forEach { action ->
+           val domain = action.enclosingElement
+            val domainName = domain.getAnnotation(Domain::class.java).name
+            if (!domainMap.containsKey(domainName)){
+                throw Exception("Not exist Name: $domain")
+            }
+
+        }
+
+
+        TODO("Not yet implemented")
+    }
+
 
 }
